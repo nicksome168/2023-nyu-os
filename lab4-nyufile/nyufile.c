5d15d9fd8ff8d3c6be3f8208da2490da62f9fc30
Nick Chao <yc6371@nyu.edu>
2023-04-30 23:17:09 -0400

    finish milestone 7

--- lab4-nyufile/nyufile.c
+++ lab4-nyufile/nyufile.c
@@ -5,6 +5,7 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/mman.h> /* mmap() is defined in this header */
+#include <openssl/sha.h>
 #include "mystructs.c"
 #include "helper.c"
 
@@ -17,11 +18,12 @@ short trav_dir(DirEntry *dir, int clusNum);
 DirEntry *get_dir_entry(int clusNum);
 DirEntry *get_next_dir(DirEntry *dir);
 int get_next_clus(int clusNum);
-void recover_file(char *fileName);
+void recover_file(char *fileName, char *sha);
 short trav_dir_recover(DirEntry *dir, int clusNum, char *recoverFileName, DirEntry *cands[]);
 int find_del_file_cand(DirEntry *dir, char *recoverFileName, DirEntry *cands[], short candNum);
 int match_del_filename(char *delFileName, char *targFileName);
-void recover(DirEntry *dirs[], char *fileName);
+void recover(DirEntry *dirs, char *fileName);
+short recover_with_sha(DirEntry *dirs[], short candNum, char *fileName, char *sha);
 
 BootEntry *bootEntry;
 unsigned char *diskAddr;
@@ -36,8 +38,8 @@ int *FAT;
 2 hrs: milestone 3
 
 how to get cluster number: https://github.com/vivisect/dissect/blob/master/dissect/formats/fat32.py
+how to read sha1 in 40 hex digits: https://stackoverflow.com/questions/47605867/how-to-make-sha1-in-c-that-returns-raw-output, https://stackoverflow.com/questions/43696184/wrong-sha1-output-in-hex
 */
-
 int main(int argc, char **argv)
 {
     if (argc < 3 || argc > 6)
@@ -45,55 +47,82 @@ int main(int argc, char **argv)
         print_usage();
         return 0;
     }
-    char *disk = argv[1];
-    read_disk(disk);
     int ch;
-    int select = 0;
-    int selectR = 0;
+    char select = 0;
+    char *fileName = NULL;
+    char *sha = NULL;
     while ((ch = getopt(argc, argv, "ilr:R:s:")) != -1)
     {
         switch (ch)
         {
         case 'i':
-            if (select == 0)
+            if (select != 0)
             {
-                select = 1;
-                list_fs_info();
+                print_usage();
+                exit(1);
             }
+            select = 'i';
             break;
         case 'l':
-            if (select == 0)
+            if (select != 0)
             {
-                select = 1;
-                list_dir_entries((int)bootEntry->BPB_RootClus);
+                print_usage();
+                exit(1);
             }
+            select = 'l';
             break;
         case 'r':
-            if (select == 0)
+            if (select != 0)
             {
-                select = 1;
-                selectR = 1;
-                recover_file(optarg);
+                print_usage();
+                exit(1);
             }
+            select = 'r';
+            fileName = optarg;
             break;
         case 'R':
-            if (select == 0)
+            if (select != 0)
             {
-                select = 1;
-                selectR = 1;
+                print_usage();
+                exit(1);
             }
+            select = 'R';
+            fileName = optarg;
             break;
         case 's':
-            if (selectR == 1)
+            if (select == 'R' || select == 'r')
+                sha = optarg;
+            else
             {
-                //     selectS = 1;
+                print_usage();
+                exit(1);
             }
             break;
         default:
             print_usage();
+            exit(1);
         }
     }
-
+    if (argv[optind] == NULL)
+    {
+        print_usage();
+        exit(1);
+    }
+    char *disk = argv[optind];
+    read_disk(disk);
+    if (select == 'i')
+        list_fs_info();
+    else if (select == 'l')
+        list_dir_entries((int)bootEntry->BPB_RootClus);
+    else if (select == 'r' || select == 'R')
+    {
+        if (select == 'R' && sha == NULL)
+        {
+            print_usage();
+            exit(1);
+        }
+        recover_file(fileName, sha);
+    }
     return 0;
 }
 
@@ -132,24 +161,29 @@ DirEntry *get_dir_entry(int clusNum)
 short trav_entries(DirEntry *dir)
 {
     short count = 0;
-    while (dir->DIR_Name[0] != 0x00 && entriesPerClus > count)
+    short allCount = 0;
+    while (dir->DIR_Name[0] != 0x00 && entriesPerClus > allCount)
     {
         char fileName[14];
+        allCount++;
         if (dir->DIR_Name[0] == 0xE5)
         {
             dir++;
             continue;
         }
         get_name(dir->DIR_Name, fileName);
+        int clusNum = dir->DIR_FstClusHI << 16 | dir->DIR_FstClusLO;
         if (dir->DIR_Attr == 0x10)
-            printf("%s/ (starting cluster = %d)\n", fileName, dir->DIR_FstClusLO);
+            printf("%s/ (starting cluster = %d)\n", fileName, clusNum);
         else
         {
             printf("%s (size = %d", fileName, dir->DIR_FileSize);
             if (dir->DIR_FileSize == 0)
                 printf(")\n");
             else
-                printf(", starting cluster = %d)\n", dir->DIR_FstClusLO);
+            {
+                printf(", starting cluster = %d)\n", clusNum);
+            }
         }
         count++;
         dir++;
@@ -160,11 +194,10 @@ short trav_entries(DirEntry *dir)
 short trav_dir(DirEntry *dir, int clusNum)
 {
     short entriesCount = 0;
-    while (dir)
+    while (1)
     {
         entriesCount += trav_entries(dir);
-        clusNum = get_next_clus(clusNum);
-        if (clusNum == -1)
+        if ((clusNum = get_next_clus(clusNum)) == -1)
             break;
         dir = get_dir_entry(clusNum);
     }
@@ -185,22 +218,33 @@ int get_next_clus(int clusNum)
     return FAT[clusNum];
 }
 
-void recover_file(char *fileName)
+void recover_file(char *fileName, char *sha)
 {
+    // printf("recovering file %s with sha %s\n", fileName, sha);
     int clusNum = (int)bootEntry->BPB_RootClus;
     DirEntry *rootDir = get_dir_entry(clusNum);
     DirEntry *cands[100];
     short candNum = trav_dir_recover(rootDir, clusNum, fileName, cands);
-    if (candNum == 1)
+    if (sha == NULL)
     {
-        recover(cands, fileName);
-        printf("%s: successfully recovered\n", fileName);
+        if (candNum == 1)
+        {
+            recover(cands[0], fileName);
+            printf("%s: successfully recovered\n", fileName);
+        }
+        else if (candNum > 1)
+        {
+            printf("%s: multiple candidates found\n", fileName);
+        }
+        else
+            printf("%s: file not found\n", fileName);
     }
-    else if (candNum == 0)
-        printf("%s: file not found\n", fileName);
     else
     {
-        printf("%s: multiple candidates found\n", fileName);
+        if (recover_with_sha(cands, candNum, fileName, sha) == 1)
+            printf("%s: successfully recovered with SHA-1\n", fileName);
+        else
+            printf("%s: file not found\n", fileName);
     }
 }
 
@@ -224,7 +268,7 @@ int find_del_file_cand(DirEntry *dir, char *recoverFileName, DirEntry *cands[], 
     while (dir->DIR_Name[0] != 0x00 && entriesPerClus > count)
     {
         get_name(dir->DIR_Name, delFileName);
-        if ((delFileName[0] == 0xE5) && match_del_filename(delFileName, recoverFileName))
+        if (((unsigned char)delFileName[0] == 0xE5) && match_del_filename(delFileName, recoverFileName))
         {
             cands[candNum] = dir;
             candNum++;
@@ -235,9 +279,8 @@ int find_del_file_cand(DirEntry *dir, char *recoverFileName, DirEntry *cands[], 
     return candNum;
 }
 
-void recover(DirEntry *dirs[], char *fileName)
+void recover(DirEntry *dir, char *fileName)
 {
-    DirEntry *dir = dirs[0];
     dir->DIR_Name[0] = fileName[0];
     int clusCount = dir->DIR_FileSize / clusSize;
     if (dir->DIR_FileSize % clusSize != 0)
@@ -249,4 +292,27 @@ void recover(DirEntry *dirs[], char *fileName)
         clusNum++;
     }
     FAT[clusNum] = 0x0ffffff8;
+}
+
+short recover_with_sha(DirEntry *dirs[], short candNum, char *fileName, char *sha)
+{
+    for (short i = 0; i < candNum; i++)
+    {
+        int clusNum = dirs[i]->DIR_FstClusHI << 16 | dirs[i]->DIR_FstClusLO;
+        unsigned char *data = dataArea + (clusNum - 2) * clusSize;
+        unsigned char hash[SHA_DIGEST_LENGTH];
+        char buf[SHA_DIGEST_LENGTH * 2];
+        size_t size = dirs[i]->DIR_FileSize;
+        SHA1(data, size, hash);
+        for (int j = 0; j < SHA_DIGEST_LENGTH; j++)
+        {
+            sprintf((char *)&(buf[j * 2]), "%02x", hash[j]);
+        }
+        if (strcmp(buf, sha) == 0)
+        {
+            recover(dirs[i], fileName);
+            return 1;
+        }
+    }
+    return 0;
 }
